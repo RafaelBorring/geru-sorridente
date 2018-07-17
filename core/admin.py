8a176eb86d3aec9710566d4ae0b0f0f94e42271c
Rafael Borring <rafaelnborring@gmail.com>
2018-07-17 16:33:36 -0300

    Alterando método de autenticação

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.hashers import make_password
 
 from core import models
 
+
+class OdontologoAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.password = make_password(obj.password)
+        obj.save()
+
+    class Media:
+        js = [
+            'jquery/js/jquery.slim.min.js',
+            'jquery/js/jquery.mask.min.js',
+            'core/js/index.js'
+        ]
+
+
+class ACSAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.password = make_password(obj.password)
+        obj.save()
+
+    class Media:
+        js = [
+            'jquery/js/jquery.slim.min.js',
+            'jquery/js/jquery.mask.min.js',
+            'core/js/index.js'
+        ]
+
+
 admin.site.register(models.Equipe)
-admin.site.register(models.Odontologo)
-admin.site.register(models.ACS)
+admin.site.register(models.Odontologo, OdontologoAdmin)
+admin.site.register(models.ACS, ACSAdmin)
 admin.site.register(models.Usuario)
 admin.site.register(models.Marcacao)
